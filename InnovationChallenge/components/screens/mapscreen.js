958895bccd1809dd69216e22271670a513d93d0b
ricozhuthegreat <ricozhuthegreat@hotmail.com>
2021-02-13 00:39:42 -0500

    Added User Location Tracking and Permissions

--- InnovationChallenge/components/screens/mapscreen.js
+++ InnovationChallenge/components/screens/mapscreen.js
@@ -1,25 +1,109 @@
 
 // mapscreen.js
 import React, { Component } from 'react';
-import { Button, View, Text } from 'react-native';
+import { Button, View, StyleSheet, Text } from 'react-native';
 
 import MapView from 'react-native-maps';
 
-export default class Mapscreen extends Component {
-  render() {
-    return (
-      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <MapView
-         style={{ flex: 1 }}
-         provider='google'
-         showsUserLocation
-         initialRegion={{
-         latitude: 37.78825,
-         longitude: -122.4324,
-         latitudeDelta: 0.0922,
-         longitudeDelta: 0.0421}}
-      ></MapView>
-      </View>
-    )
-  }
+export const getCurrentLocation = () => {
+    return new Promise((resolve, reject) => {
+      navigator.geolocation.getCurrentPosition(position => resolve(position), e => reject(e));
+    });
+};
+  
+var UserLocation = {
+    latitude: 0,
+    longitude: 10,
+    latitudeDelta: 0.003,
+    longitudeDelta: 0.003,
 }
+
+export default class Mapscreen extends Component {
+
+    constructor (props) {
+        super(props);
+        this.states = {
+          region: UserLocation
+        }
+    }
+
+    componentDidMount() {
+        return getCurrentLocation().then(position => {
+          if (position) {
+            this.states.region = {
+                latitude: position.coords.latitude,
+                longitude: position.coords.longitude,
+                latitudeDelta: 0.02,
+                longitudeDelta: 0.04,
+              },
+              executed = true;
+              
+              this.forceUpdate();
+          }},);
+    }
+
+    render() {
+        return (
+            <View style={styles.container}>
+            <MapView style={StyleSheet.absoluteFillObject}
+                provider = 'google'
+                showsUserLocation
+                region = {this.states.region}>
+            </MapView>
+            </View>
+            )
+        }
+    }
+
+function toUser({}){
+    return getCurrentLocation().then(position => {
+      if (position) {
+          UserLocation = {
+            latitude: position.coords.latitude,
+            longitude: position.coords.longitude,
+            latitudeDelta: 0.003,
+            longitudeDelta: 0.003,
+          },
+        console.log(1)
+      }
+    },
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      alignItems: 'center',
+    },
+    developmentModeText: {
+      marginBottom: 20,
+      color: 'rgba(0,0,0,0.4)',
+      fontSize: 14,
+      lineHeight: 19,
+      textAlign: 'center',
+    },
+    contentContainer: {
+      paddingTop: 30,
+    },
+    overlay: {
+      position: 'absolute',
+      bottom: 50,
+      paddingVertical: 10,
+        paddingHorizontal: 30,
+        backgroundColor: "#007bff",
+        borderColor: "#eee",
+        borderRadius: 5,
+        elevation: 10
+    },
+    overlay2: {
+      position: 'absolute',
+      right: 20,
+      bottom: 50,
+      paddingVertical: 8,
+        paddingHorizontal: 10,
+        backgroundColor: "white",
+        borderColor: "#eee",
+        borderRadius: 5,
+        elevation: 10
+    },
+  });
